a652786750bc6bc05ec4ab570d1647d1f0c31f69
StefanPanait <stefanapanait@gmail.com>
2014-08-05 00:15:34 -0400

    no message

--- app.js
+++ app.js
@@ -1,61 +1,13 @@
-"use strict";
-var express = require('express'),
-    exphbs = require('express3-handlebars'),
-    database = require('./db'),
-    app = express();
+var express = require('express')
+var app = express();
 
-app.set('views', __dirname + '/views');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(req, res) {
-    console.log("root");
-    res.send("<a href='/'>Click here</a> in a minute, server is re-starting ");
-    /*    var dayCount = database.getDayCount();
-    database.countMailingtable(function(err, emailCount) {
-        database.updateReport(function(domains) {
-            res.render('report', {
-                domains: domains,
-                emailCount: emailCount,
-                dayCount: dayCount
-            });
-        });
-    });*/
-});
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+})
 
-app.get('/passTime', function(req, res) {
-    console.log("passing time");
-    database.passTime(function() {
-        database.updateDomainsTable(function() {
-            res.redirect("/");
-
-        });
-    });
-
-});
-
-app.get('/updateReport', function(req, res) {
-    res.redirect("/");
-});
-
-
-app.get('/emails', function(req, res) {
-    database.getAllEmails(function(err, rows) {
-        if (err) console.log(err);
-        res.render("emails", {
-            emails: rows
-        });
-    });
-});
-
-app.get('/clearSimulation', function(req, res) {
-    //database.createTables();
-    res.send("<a href='/'>Click here</a> in a minute, server is re-starting ");
-});
-var server = app.listen(3000, function() {
-    //console.log("init db");
-    //database.init();
-    console.log('Listening on port %d', server.address().port);
-});
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
