aad607f26e77643e44392bf35465b6b2517e8955
StefanPanait <stefanapanait@gmail.com>
2014-08-05 00:18:23 -0400

    no message

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
-var app = express();
+var app = express(),
+    exphbs = require('express3-handlebars'),
+    database = require('./db');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
