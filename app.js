26809e541af93608be4a2d6b79757a9fecbc1668
StefanPanait <stefanapanait@gmail.com>
2014-08-05 00:25:10 -0400

    no message

--- app.js
+++ app.js
@@ -59,4 +59,5 @@ app.get('/clearSimulation', function(req, res) {
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
+    database.init();
 })
